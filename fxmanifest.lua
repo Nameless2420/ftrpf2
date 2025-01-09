cad57dba712c1dc72756170ef980cb33a1d89588
Stoneyyy6 <acole6454@gmail.com>
2025-01-09 15:33:30 -0600

    1

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with Durty Cloth Tool v3.1.0.3 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_ftrpf2_shop.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_ftrpf2_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
